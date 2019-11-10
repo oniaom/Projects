59d116995f1a42f135ebedaa419212a9f16af94b
oniaom <56845674+oniaom@users.noreply.github.com>
2019-11-10 14:40:31 +0200

    Add files via upload

--- Project Program Downloader/Program Downloader.py
+++ Project Program Downloader/Program Downloader.py
@@ -2,11 +2,16 @@
 from appJar import gui
 import wget
 import urllib.error
+import threading
 
 app = gui()
 CheckedBoxes = []  # Array of which checkboxes were checked
+success = False
+firstItteration = True
 
 def ButtonClick():
+    global firstItteration
+    firstItteration = False
     selectedPrograms = ""  # We check which boxes were checked, append them to CheckedBoxes, and put them in a string with dashes.
     if(app.getCheckBox("cbBrowserFirefox")):
         CheckedBoxes.append("firefox")
@@ -41,17 +46,23 @@ def ButtonClick():
     # Since the last dash is going to cause us problems, we have to remove it.
     RemoveLastDash = selectedPrograms.rfind("-")
     selectedPrograms = selectedPrograms[:RemoveLastDash]+""+selectedPrograms[RemoveLastDash+1:]
-    DownloadProgs(selectedPrograms)
-
+    
+    DownloadProgs(selectedPrograms)    
+    
+    app.removeAllWidgets()  #  These 2 lines are essential for appJar, because it tries to re-add the widgets causing errors
+    
+    mainFunction()          #  even without calling mainFunction()
 def DownloadProgs(Programs): #This just calls wget to download what we want.
+    global success
     try:
-        wget.download(url="https://www.ninite.com/"+Programs+"/ninite.exe")
-
+        wget.download(url="https://www.ninite.com/"+Programs+"/ninite.exe")       
+        success = True
     except(urllib.error.HTTPError): # In case the url isn't found (user selects nothing, ninite changes their site)
-        print("The url wasn't found.")
+        success = False
 
-if __name__ == "__main__":   
+def mainFunction():
      # These are the selections the user has.
+    CheckedBoxes.clear()
     app.addCheckBox("cbBrowserFirefox",row=0,column=0,name="Firefox")
     app.addCheckBox("cbBrowserChrome",row=0,column=1,name="Chrome")
     app.addCheckBox("cbLibreOffice",row=1,column=0,name="LibreOffice")
@@ -64,8 +75,20 @@ if __name__ == "__main__":
     app.addCheckBox("cbVSCode",row=4,column=0,name="VSCode")
     app.addCheckBox("cbVLC",row=4,column=1,name="VLC")
     app.addCheckBox("cbCodecs",row=4,column=2,name="K-Lite Codecs")
-    app.addCheckBox("cbSpotify",row=4,column=3,name="Spotify")
+    app.addCheckBox("cbSpotify",row=4,column=3,name="Spotify")    
     # We link our Button to the function ButtonClick so when the user clicks the button, the function starts.
     app.addButton("Download",ButtonClick)
+    global success
+    global firstItteration
+    if (success == True):
+        app.addLabel("lSuccess",text="Success!\nYour programs have\ndownloaded!",row=6,column=0)
+    elif success == False and firstItteration == True:
+        pass
+    elif success == False and firstItteration == False:
+        app.addLabel("lFailure","The url wasn't found!\nPlease check your\ninternet connection and\ntry again!")
     app.go()
+
+if __name__ == "__main__":   
+    mainFunction()
+    
     
