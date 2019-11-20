e27bb22e048c89a2efae69b245ae2ee4e252749d
oniaom <56845674+oniaom@users.noreply.github.com>
2019-11-20 15:56:51 +0200

    Add files via upload

--- Project Timespamps/TimeStamps with Box Editing.py
+++ Project Timespamps/TimeStamps with Box Editing.py
@@ -19,63 +19,43 @@ for line in timeFile: #Get everything from file
 
 for indexes in range(len(timestamps)): #Remove the \n from string
 	if "\n" in timestamps[indexes]:
-				 timestamps[indexes]=timestamps[indexes].rstrip('\n')
+		timestamps[indexes]=timestamps[indexes].rstrip('\n')
 
 timestrings = timestamps
-timea=[]
-timestemp=0
-
-for times in timestamps:
-	a=times.split("- ")
-	timea.append(a[1])
-
-
 
 for times in timestamps: #Make a list of times
     	a=([int(b) for b in times.split() if b.isdigit()])
     	time.append(a)
-print(time)
 i=0
 index=0
-while index < (len(time)):
-	print("time=",time[index])
-	print("len=",len(time[index]))
+
+while index < (len(time)): #Find if the user typed numbers before the intended timestamp and remove them
 	while(len(time[index]) >2):
-		print("index=",time[index])
 		temp = time[index][-1],time[index][-2]
-		print("testy too",temp)
 		time.remove(time[index])
-		print("done")
-		time.insert(0,temp)
+		time.insert(index,temp)
 	index+=1
-print(time)
+
 for timeIndexes in range(len(time)): #Make time in proper format ex 3:30
 	while i<2:
 		hi=str(time[timeIndexes][i])
 		cutinhalf = len(hi)//2
 		if len(hi) == 4:
-			print(hi+" lenght 4")
 			hi=hi[:cutinhalf]+":"+hi[-cutinhalf:]
 		elif len(hi) <=3:
-			print(hi+" lenght 3")
 			if hi=="0":
 				hi="0:00"
 			else:
 				hi=hi[:1]+":"+hi[1:]
-
 		elif len(hi) > 6:
 			hi=hi+" Try a number below 6 digits."
-		
 		else:
 			newcut=cutinhalf//2
 			testcut = (cutinhalf/2) /2
 			if testcut > 0.6 < 1:
-				print(testcut)
 				hi=hi[:newcut+1]+":"+hi[newcut+1:cutinhalf+1]+":"+hi[cutinhalf+1:]
 			else:
 				hi=hi[:newcut]+":"+hi[newcut:cutinhalf+1]+":"+hi[cutinhalf+1:]
-	
-		print(hi)
 		timeproper.append(str(hi))
 		i+=1
 	i=0
