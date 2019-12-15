ea5e35219ae55ae934aa7c62a4f18ae3997257da
oniaom <56845674+oniaom@users.noreply.github.com>
2019-12-15 11:30:47 +0300

    Update Number Swapper.py

--- Project Song Numbering/Number Swapper.py
+++ Project Song Numbering/Number Swapper.py
@@ -2,36 +2,49 @@ from appJar import gui
 import os
 
 
-def getNumber(file):
-    numberInFile = file[:3]
-    return numberInFile
+def calculateNumber(name):
+    numberTest = 0
+    while True:
+        try:
+            int(name[numberTest])
+            numberTest += 1
+        except ValueError:
+            return numberTest
+
+
+def getNumber(name):
+    return name[:calculateNumber(name)]
 
 
 def getAndSwap(name, number):
-    name = name[3:]
-    name = number + name
-    return name
+    return number + name[calculateNumber(name):]
 
 
 def getDir(listDir):
     directory = "/".join(listDir)
-    directory += "/"
-    return directory
+    return directory + "/"
 
 
 def numberAction():
-    fileToSwap = prog.getEntry("file1")  # Get the first and second files to swap numbers
-    fileToGo = prog.getEntry("file2")
+    firstFile, secondFile = prog.getEntry("file1"), prog.getEntry(
+        "file2")  # Get the first and second files to swap numbers
+    both = [firstFile, secondFile]  # This list contains both files with the original directory
 
-    both = [fileToSwap, fileToGo] # This list contains both files with the original directory
-    swapper = [getNumber(fileToGo.split('/')[-1]), getNumber(fileToSwap.split('/')[-1])] # this contains the numbers of the files
+    for files in both:
+        if '.mp3' not in files:
+            prog.errorBox(title="Error", message="One or both files are not .mp3!")
+            return
 
-    for stuff in range(len(both)):  # So now we iterate through the files
-        name = both[stuff].split('/')[-1]  # This is the actual name.
-        newName = getAndSwap(name, swapper[stuff])  # This swaps the numbers from file1 and file2
-        listDirectory = both[stuff].split('/')[:-1]  # Creates a list containing the path
+    swapper = [getNumber(secondFile.split('/')[-1]),
+               getNumber(firstFile.split('/')[-1])]  # this contains the numbers of the files
+
+    for files in range(len(both)):  # So now we iterate through the files
+        name = both[files].split('/')[-1]  # This is the actual name. (Splits and returns the first index)
+        newName = getAndSwap(name, swapper[files])  # This swaps the numbers from file1 and file2
+        listDirectory = both[files].split('/')[
+                        :-1]  # Creates a list containing the path (skips the last index  which is the name)
         directory = getDir(listDirectory)  # And makes it a string with "/"
-        os.rename(both[stuff], directory + newName)  # Finally, we rename the file!
+        os.rename(both[files], directory + newName)  # Finally, we rename the current file!
 
 
 prog = gui("Song Swapper")
