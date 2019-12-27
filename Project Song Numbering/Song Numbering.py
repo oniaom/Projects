9f063ed19e8bb6cc4840daefc7329d12fe49864a
oniaom <56845674+oniaom@users.noreply.github.com>
2019-12-27 14:49:50 +0300

    Update Song Numbering.py

--- Project Song Numbering/Song Numbering.py
+++ Project Song Numbering/Song Numbering.py
@@ -1,28 +1,37 @@
 from appJar import gui
 import os
 
+def calculateNumber(name):
+    numberTest = 0
+    while True:
+        try:
+            int(name[numberTest])
+            numberTest += 1
+        except ValueError:
+            return numberTest
+
 
 def isInt(string):
     string = string[:2]  # Get the first character of the string
     try:  # if it's an int, return true
-        if int(string):
-            return True
+        int(string)
+        return True
     except ValueError:  # if not, return false
         return False
 
 
-def increment(string, increment):
-    numberInString = int(string[:2])
-    stringWithoutNumber = string[3:]
-    if int(numberInString + increment) < 10:
-        return "0" + str(numberInString + increment) + " " + stringWithoutNumber
+def increment(string, number):
+    numberInString = int(string[:calculateNumber(string)])
+    stringWithoutNumber = string[calculateNumber(string):]
+    if int(numberInString + number) < 10:
+        return "0" + str(numberInString + number) + " " + stringWithoutNumber
     else:
-        return str(numberInString + increment) + " " + stringWithoutNumber
+        return str(numberInString + number) + stringWithoutNumber
 
 
 def insertNumber(string, numToInsert):
     if numToInsert < 10:
-        return str(0) + str(numToInsert) + f" - {string}"
+        return "0" + str(numToInsert) + f" - {string}"
     else:
         return str(numToInsert) + f" - {string}"
 
